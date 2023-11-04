e9779bccc5a078d5143479553b4ff4feda4bdc1d
Maddy Boussa <mboussa@nycap.rr.com>
2023-11-04 17:37:28 -0400

    added project into this directory

---
+++ Particle System/Particle System/ParticleSystem.cpp
@@ -0,0 +1,142 @@
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "ParticleSystem.h"
+#include <iostream>
+
+ParticleSystem::ParticleSystem(int _numParticles)
+{
+	numParticles = _numParticles;	// Set the number of particles
+
+	// Allocate memory for positions, velocities, colors, and lifetimes.
+	positions = new float[numParticles * 3];
+	velocities = new float[numParticles * 3];
+	colors = new float[numParticles * 4];
+	lifeTimes = new float[numParticles];
+
+
+	for (int i = 0; i < numParticles; i++) {
+		// Initialize the life times
+		lifeTimes[i] = maxLifeTime - maxLifeTime * i / numParticles;
+
+		// initialize velocities to a random value within the velocity range
+		velocities[i * 3 + 0] = getRandomValue(minSpeedX, maxSpeedX);	// x
+		velocities[i * 3 + 1] = getRandomValue(minSpeedY, maxSpeedY);	// y
+		velocities[i * 3 + 2] = getRandomValue(minSpeedZ, maxSpeedZ);	// z
+
+		// initialize positions at the origin
+		positions[i * 3 + 0] = 0.0f;	// x
+		positions[i * 3 + 1] = 0.0f;	// y
+		positions[i * 3 + 2] = 0.0f;	// z
+
+		// initialize colors as solid blue
+		colors[i * 4 + 0] = 0.0f;	// red
+		colors[i * 4 + 1] = 0.0f;	// green
+		colors[i * 4 + 2] = 1.0f;	// blue
+		colors[i * 4 + 3] = 1.0f;	// alpha
+
+	}
+}
+
+void ParticleSystem::update(float deltaTime)
+{
+	for (int i = 0; i < numParticles; i++) {
+
+		// Update lifetime, velocity, position, and color.
+
+		lifeTimes[i] += deltaTime;
+
+		// velocity
+		// x and z velocities should remain the same
+		// alter y velocity based on gravity
+		velocities[i * 3 + 1] += acceleration[1] * deltaTime;
+
+		// incremement position based on velocity
+		positions[i * 3 + 0] += velocities[i * 3 + 0] * deltaTime;
+		positions[i * 3 + 1] += velocities[i * 3 + 1] * deltaTime;
+		positions[i * 3 + 2] += velocities[i * 3 + 2] * deltaTime;
+
+		// color gradually fades
+		// only decrement alpha indices
+		colors[i * 4 + 3] = 1 - (lifeTimes[i] / maxLifeTime);
+
+
+		// Reset particle states (positions, velocities, colors, and lifetimes) when the lifetime reaches the maxLifeTime
+
+		if (lifeTimes[i] >= maxLifeTime)
+		{
+			// reset lifetimes
+			lifeTimes[i] = 0.0f;
+
+			// reset velocities
+			velocities[i * 3 + 0] = getRandomValue(minSpeedX, maxSpeedX);	// x
+			velocities[i * 3 + 1] = getRandomValue(minSpeedY, maxSpeedY);	// y
+			velocities[i * 3 + 2] = getRandomValue(minSpeedZ, maxSpeedZ);	// z
+
+			// reset positions to the origin
+			positions[i * 3 + 0] = 0.0f;	// x
+			positions[i * 3 + 1] = 0.0f;	// y
+			positions[i * 3 + 2] = 0.0f;	// z
+
+			// reset alpha to 1
+			colors[i * 4 + 3] = 1.0f;	// alpha
+		}
+	}
+}
+
+void ParticleSystem::draw()
+{
+
+	// immediate mode drawing
+	// loop through all particles
+	for (unsigned int i = 0; i < numParticles; i++)
+	{
+		glPointSize(3.0f);
+		glBegin(GL_POINTS);
+		glColor4f(colors[i * 4 + 0], colors[i * 4 + 1], colors[i * 4 + 2], colors[i * 4 + 3]);
+		glVertex3f(positions[i * 3 + 0], positions[i * 3 + 1], positions[i * 3 + 2]);
+		glEnd();
+	}
+
+	// VBO mode drawing
+	//glBindBuffer(GL_ARRAY_BUFFER, pos_id);
+	//glEnableClientState(GL_VERTEX_ARRAY);
+	//glVertexPointer(3, GL_FLOAT, 0, NULL);
+
+	//glBindBuffer(GL_ARRAY_BUFFER, col_id);
+	//glEnableClientState(GL_COLOR_ARRAY);
+	//glColorPointer(4, GL_FLOAT, 0, NULL);
+
+
+	//// draw elements
+	//glDrawArrays(GL_POINTS, 0, numParticles);
+
+	//
+	//glDisableClientState(GL_VERTEX_ARRAY);
+	//glDisableClientState(GL_COLOR_ARRAY);
+}
+
+void ParticleSystem::prepareBufObjects()
+{
+	// generate position buffer object
+	glGenBuffers(1, &pos_id);
+
+	// bind VBO in order to use
+	glBindBuffer(GL_ARRAY_BUFFER, pos_id);
+
+	// upload data to VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numParticles * 3, positions, GL_STATIC_DRAW);
+
+	// clean up 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// generate color buffer object
+	glGenBuffers(1, &col_id);
+	glBindBuffer(GL_ARRAY_BUFFER, col_id);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numParticles * 4, colors, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+float ParticleSystem::getRandomValue(float min_value, float max_value)
+{
+	return min_value + (std::rand()) / (RAND_MAX / (max_value - min_value));;
+}
